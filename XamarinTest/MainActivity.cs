e5b19c30acc9bc4d946f11eca31b8181355c9888
JimWilcox3 <jim.wilcox@aimovers.com>
2016-06-22 09:40:07 -0400

    Initial Version

---
+++ XamarinTest/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Akavache;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace XamarinTest
+{
+    [Activity(Label = "XamarinTest", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            BlobCache.ApplicationName = "XamarinTest";
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+            Task.Run(() => TestAsync());
+
+        }
+
+        private async void TestAsync()
+        {
+            await BlobCache.LocalMachine.InsertObject("TestKey", "TestValue", DateTimeOffset.Now.AddDays(3));
+
+            string s = await BlobCache.LocalMachine.GetObject<string>("TestKey");
+        }
+
+    }
+}
+
